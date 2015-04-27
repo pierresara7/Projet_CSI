7137481a3e3b3ce48ae0503f209a8d82604f38eb
Lahat <bamba34@hotmail.com>
2015-04-27 20:42:40 +0200

    ..

--- form_inscription.php
+++ form_inscription.php
@@ -15,9 +15,8 @@
 
 <?php 
 session_start();
-// inclure la page html de connexion
 if (isset($_SESSION['login'])) {
-require("form_connexion.html");
+require("form_connexion.php");
 }
 else{
 	echo $_SESSION['login'] ;
@@ -26,24 +25,20 @@ else{
  ?>
 
 <form action="inscription.php" method="POST" onSubmit="return verifForm()">
-			<span class="label">Login</span>
-			<input type="text" name="login" size="20" onblur="verifLogin(this)"/>
-			<br/>
-			<span class="label">Mot de passe</span>
-			<input type="password" name="mdp" onblur="verifMdp(this)"/>
-			<br/>
-			<span class="label">Confirmation</span>
-			<input type="password" name="mdp2" onblur="confirmationMdp(mdp,this)"/>
-			<br/>
-			<span class="label">Adresse email</span>
-			<input type="text" name="email" onblur="verifMail(this)"/>
-		  <br>
-		  
-		    <input type="image" class="img_connexion"src="image/inscription.jpg" value="submit" align="middle"/>
-		  </div>
+<div id="contact">
+<form method="post" action="#">
+    <h2 align="center"> Inscription Client </h2>
+    <pre>
 
-			
-		</form>
+         Login 			: <input name="login" type="text" style="height:30px" placeholder="Votre Login"/>        	    Nom : <input name="nom" type="text" style="height:30px" placeholder="Votre NOM"/>
 
+	   Prenom 		: <input name="prenom" type="text" style="height:30px" placeholder="Votre Prenom"/>       	Adresse	: <input type="text" name="adressse" style="height:30px" placeholder="Votre Adresse"/>
+	   
+	   Tel    		: <input type="text" name="tel" style="height:30px" placeholder="Votre Telephone"/> 	   EMAIL	: <input name="email" type="email" style="height:30px" placeholder="Votre MAIL"/> 
+
+	   Sexe 		: <input name="sexe" type="text" style="height:30px" placeholder="Votre Sexe"/>         Mot de Passe : <input type ="password" name="mdp" onblur="verifMdp(this)"/>
+
+				  <input type="image" class="img_connexion"src="image/inscription.jpg" value="Valider et Enregistrer" align="middle"> 
+    </pre>
 	</body>
 	</html>

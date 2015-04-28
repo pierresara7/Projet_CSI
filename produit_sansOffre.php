99a327ab7513edf3661d8ce694e8e1f24f73170b
alphonse <pierresara7@gmail.com>
2015-04-28 19:22:33 +0200

    truc

--- produit_sansOffre.php
+++ produit_sansOffre.php
@@ -1,7 +1,7 @@
 <?php
 include("connexion_database.php");
 include("header.php");
-$req=mysql_query("SELECT id_Prod,nom_Prod FROM produit where a_reduction=FALSE");
+$req=mysql_query("SELECT id_Prod,nom_prod FROM produit where a_reduction=0");
 	if($req){
 		echo"<table class='span8'>
 				<tr bgcolor='#CCCCCC'>
@@ -9,7 +9,6 @@ $req=mysql_query("SELECT id_Prod,nom_Prod FROM produit where a_reduction=FALSE")
 				<th align='center'>Offre reductionnnelle</th>
 				<th align='center'>Offre promotionnelle</th>
 
-				<th align='center' colspan='2'>Faire</th>
 				</tr>";
 				
  // use bgcolor ds les tr
@@ -17,7 +16,9 @@ $req=mysql_query("SELECT id_Prod,nom_Prod FROM produit where a_reduction=FALSE")
     		while($l= mysql_fetch_array($req)){
 				echo"<tr>
 					<td align='center'>".$l['nom_prod']."</td>
-					";?><td>
+					";?>
+					<td>
+					<form method="POST" action="beneficie_OR.php">
 					<select name="liste">
 <?php
 $sql='SELECT id_OffreReduc,pourcentageOR from Offre_reductionnelle';
@@ -26,6 +27,19 @@ while ($data = mysql_fetch_array($list))
      {echo'<option value="'.$data['id_OffreReduc'].'">'.$data['pourcentageOR'].'</option>';}
 ?>
 </select>
+<input type="submit" name="go" value="Valider" class="btn btn-danger">
+					</td>
+					<td>
+					<form method="POST" action="beneficie_OP.php">
+					<select name="liste">
+<?php
+$sql2='SELECT idOP,pourcentageOP from Offre_promotionnelle';
+$list2 = mysql_query($sql2);
+while ($data2 = mysql_fetch_array($list2))
+     {echo'<option value="'.$data2['idOP'].'">'.$data2['pourcentageOP'].' %</option>';}
+?>
+</select>
+<input type="submit" name="go" value="Valider" class="btn btn-danger">
 					</td>
 					<?php
 					echo "
--- valider_panier.php
+++ valider_panier.php
@@ -1,8 +1,8 @@
 <?php
 include("connexion_database.php");
 	$idpanier=$_POST['idpanier'];
-	$dateDuJour=
 $reqq=mysql_query("UPDATE panier Set statut_panier='Valide' Where idpanier=$idpanier");
 $reqq2=mysql_query("SELECT montant_ttc FROM panier Where idpanier=$idpanier");
+$montant_panier=mysql_result($reqq2,0);
 include("generer_bilan.php");
 ?>
